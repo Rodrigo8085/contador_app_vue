185682eeed9e87956e76799d7d368057efda8133
Rodrigo Santiago <rsantiago643@gmail.com>
2020-12-27 11:54:18 -0600

    Init

---
+++ src/components/Contador.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+ @Component
+export default class Contador extends Vue {
+  valueNumber: number = 0;
+ get formattedNumber() {
+    return `El valor total es:  ${this.valueNumber}`;
+  }
+increase() {
+    this.valueNumber += 1;
+  }
+ decrease() {
+    this.valueNumber -= 1;
+  }
+}
+</script>
+<template>
+  <div>
+    <fieldset>
+      <legend>{{ formattedNumber }}</legend>
+        <button @click="increase">Increase</button>
+        <button @click="decrease">Decrease</button>
+    </fieldset>
+  </div>
+</template>
